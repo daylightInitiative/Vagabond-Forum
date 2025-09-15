488fda4755db3c103094b4ba54c60dc09886b1be
daylightInitiative <202697339+daylightInitiative@users.noreply.github.com>
2025-09-15 19:58:06 -0400

    fixed the dynamic replacement of userid->username for replies finally, minor changes

--- sql/query_page_replies.sql
+++ sql/query_page_replies.sql
@@ -1,2 +1,5 @@
-SELECT * FROM replies WHERE parent_post_id = %s
-    ORDER BY creation_date DESC
+SELECT r.*, u.username AS author_username
+FROM replies AS r
+LEFT JOIN users u ON u.id = r.author
+WHERE r.parent_post_id = %s
+ORDER BY r.creation_date DESC;
--- sql/view_post_by_id.sql
+++ sql/view_post_by_id.sql
@@ -1,1 +1,4 @@
-SELECT * FROM posts WHERE id = %s
+SELECT p.*, u.username as author_username
+FROM posts as p
+LEFT JOIN users u ON u.id = p.author
+WHERE p.id = %s
